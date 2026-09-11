36b5ebfac27ba533842fe42e8e8d2a9a0f4999a7
Yashrajpawar-dev <yashrajpawar9604@gmail.com>
2026-09-11 19:32:00 +0530

    Implement class structure for Customer and Vehicle, add build configuration and .gitignore

---
+++ include/Customer.h
@@ -0,0 +1,23 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <string>
+
+using namespace std;
+
+class Customer {
+private:
+    int customerId;
+    string name;
+    string email;
+    string phone;
+    string password;
+
+public:
+    Customer();
+
+    void inputCustomer();
+    void displayCustomer() const;
+};
+
+#endif
